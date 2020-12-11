7de6e80f1fab8e6fe95f8bdd58b6de583fb37061
cmmicustomer <cmmicustomer@163.com>
2020-12-11 18:31:45 +0800

    去掉注释

--- appium_test.py
+++ appium_test.py
@@ -12,13 +12,13 @@ desired_capabilities['resetKeyboard'] = True
 desired_capabilities['noReset'] = True
 desired_capabilities['fullReset'] = False
 print(desired_capabilities)
-# driver = webdriver.Remote('http://localhost:4723/wd/hub',desired_capabilities)
-# driver.implicitly_wait(10)
-# driver.find_element_by_id('com.android.calculator2:id/digit_1').click()
-# time.sleep(1)
-# driver.find_element_by_accessibility_id('加').click()
-# time.sleep(1)
-# driver.find_element_by_id('com.android.calculator2:id/digit_3').click()
-# time.sleep(1)
-# driver.find_element_by_accessibility_id('等于').click()
+driver = webdriver.Remote('http://localhost:4723/wd/hub',desired_capabilities)
+driver.implicitly_wait(10)
+driver.find_element_by_id('com.android.calculator2:id/digit_1').click()
+time.sleep(1)
+driver.find_element_by_accessibility_id('加').click()
+time.sleep(1)
+driver.find_element_by_id('com.android.calculator2:id/digit_3').click()
+time.sleep(1)
+driver.find_element_by_accessibility_id('等于').click()
 
